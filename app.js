d3154b9a397c2fc17440aba3567910b8e68a15f9
emjose <emjose@gmail.com>
2021-02-03 14:05:46 -0500

    hex setup

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["cyan", "yellow", "pink", "rgba(50,70,800)", "#E2CFFF", "#33FFD7", "red", "rgba(120,15,400)", "#FF5733", "orange", "#FCC370", "#70E3FC", "#FFD2F2", "#CFCFCF", "#000000"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    // get random number based on example array above
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
