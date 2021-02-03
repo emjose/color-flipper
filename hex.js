ae4d990d94321e5e8f805e04d5ef14ee53a425a4
emjose <emjose@gmail.com>
2021-02-03 15:30:01 -0500

    more styling

--- hex.js
+++ hex.js
@@ -1,5 +1,7 @@
+// generate 6-character hex values
+// use .floor to get whole integer values in the array
+
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-// generates 6-character hex values
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
@@ -16,3 +18,4 @@ btn.addEventListener("click", function () {
 
 function getRandomNumber() {
   return Math.floor(Math.random() * hex.length);
+}
